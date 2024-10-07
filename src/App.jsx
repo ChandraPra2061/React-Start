6fe068d2f5c4a0693fad73433b6d95a2f619622c
ChandraPra2061 <chandraprakashchaurasiya2061@gmail.com>
2024-10-07 19:47:13 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import CoreConcepts from "./assets/components/CoreConcepts";
+import Examples from "./assets/components/Examples";
+import Header from "./assets/components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
